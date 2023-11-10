7185456794a5db43bf392242a6d68e70779ed28f
Welliton Gervickas <wellitogervickas@gmail.com>
2023-11-10 14:05:54 -0300

    fix: try to pack only production

--- config/networks.ts
+++ config/networks.ts
@@ -1,7 +1,16 @@
-export const networks = {
-  mumbai: {
-    url: `https://polygon-mumbai.g.alchemy.com/v2/${process.env.MUMBAI_API_KEY}`,
-    chainId: 80001,
-    accounts: [String(process.env.ACCOUNTS_PRIVATE_KEY_1)]
-  }
+const networks = {}
+
+if (process.env.NODE_ENV !== 'devevelopment') {
+  Object.assign(networks, {
+    mumbai: {
+      url: `https://polygon-mumbai.g.alchemy.com/v2/${process.env.MUMBAI_API_KEY}`,
+      chainId: 80001,
+      accounts: [
+        String(process.env.ACCOUNTS_PRIVATE_KEY_1),
+        String(process.env.ACCOUNTS_PRIVATE_KEY_2)
+      ]
+    }
+  })
 }
+
+export { networks }
