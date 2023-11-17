8aa9a96278060ca515b813630e688e04b060dfc5
welliton gervickas <wellitogervickas@gmail.com>
2023-11-16 22:38:38 -0300

    feat: adding new specs

--- test/bridge/Bridge.spec.ts
+++ test/bridge/Bridge.spec.ts
@@ -8,15 +8,36 @@ import { deployAccessManagementFixture } from '@/test/accessManagement/fixtures'
 import { deployBridgeFixture } from './fixture'
 
 describe('Bridge', function () {
-  it('should return adapter address', async function () {
-    let accessManagement: AccessManagement
-    let accessManagementAddress: string
+  let accessManagement: AccessManagement
+  let accessManagementAddress: string
 
-    beforeEach(async function () {
-      // initialize access management fixture
-      const fixture = await loadFixture(deployAccessManagementFixture)
-      accessManagement = fixture.accessManagement
-      accessManagementAddress = fixture.accessManagementAddress
-    })
+  beforeEach(async function () {
+    // initialize access management fixture
+    const fixture = await loadFixture(deployAccessManagementFixture)
+    accessManagement = fixture.accessManagement
+    accessManagementAddress = fixture.accessManagementAddress
+  })
+
+  it('should return adapter as zero address if not set', async function () {
+    const adapterAddress = ethers.ZeroAddress
+    const fixture = await loadFixture(
+      deployBridgeFixture.bind(this, accessManagementAddress)
+    )
+
+    const adapter = await fixture.bridge.adapter()
+    expect(adapter).to.be.equal(adapterAddress)
+  })
+
+  it('should set adapter address', async function () {
+    const adapterAddress = '0x00000000000000000000000000000000000000D2'
+
+    const { bridge } = await loadFixture(
+      deployBridgeFixture.bind(this, accessManagementAddress)
+    )
+
+    await bridge.setAdapter(adapterAddress)
+
+    const adapter = await bridge.adapter()
+    expect(adapter).to.be.equal(adapterAddress)
   })
 })
