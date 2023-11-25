661c32eea502529672743b74d71c4731ab13ff95
welliton gervickas <wellitogervickas@gmail.com>
2023-11-25 17:20:21 -0300

    feat: adding roles

--- scripts/constants.ts
+++ scripts/constants.ts
@@ -1,2 +1,8 @@
 export const DEVELOPER_ROLE = 1n
 export const DEVELOPER_ROLE_DELAY = 0n
+
+export const ROUTER_ROLE = 2n
+export const ROUTER_ROLE_DELAY = 0n
+
+export const ADAPTER_ROLE = 3n
+export const ADAPTER_ROLE_DELAY = 0n
